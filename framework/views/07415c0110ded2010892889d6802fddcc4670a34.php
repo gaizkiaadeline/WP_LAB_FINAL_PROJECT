c0cb83b23b530e978eea53e3eef549837c17397f
Gaizkia Adeline Atmaka <90249058+gaizkiaadeline@users.noreply.github.com>
2023-11-23 11:21:09 +0700

    Add files via upload

---
+++ framework/views/07415c0110ded2010892889d6802fddcc4670a34.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">XiAO DiNGDoNG</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Search Food</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Cart</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Welcome, Xiao User
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="#">Transaction History</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Sign Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+<?php /**PATH C:\Users\HP\Desktop\WebProgramming_LAB_Project\WebProgramming_LAB_Project\resources\views/layouts/navbarMember.blade.php ENDPATH**/ ?>
---
+++ framework/views/65b1ee7fe609d535bef58d2c3114eb6419f0a9f7.php
@@ -0,0 +1,35 @@
+
+
+<?php $__env->startSection('title', 'Register'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+    <div class="container-fluid w-50 d-flex justify-content-center">
+        <p class="" style="background-color: #f5ec9a;">Register</p>
+    </div>
+    <div class="form-group container-fluid w-50 mb-3" style="background-color: #f5ec9a;">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="form-group container-fluid w-50 mb-3" style="background-color: #f5ec9a;">
+        <label for="exampleFormControlInput2" class="form-label">Username</label>
+        <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Minimum 5 chars, maximum 50 chars">
+    </div>
+    <div class="form-group container-fluid w-50 mb-3" style="background-color: #f5ec9a;">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+    </div>
+    <div class="form-group container-fluid w-50 mb-3" style="background-color: #f5ec9a;">
+        <label for="exampleInputPassword2">Confirm Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" name="password_confirmation">
+    </div>
+    <div class="form-group container-fluid w-50 mb-3" style="background-color: #f5ec9a;">
+        <input class="btn btn-primary w-100" type="submit" value="Register">
+    </div>
+    <div class="container-fluid w-50 d-flex justify-content-center">
+        <label>Already have an account?</label>
+        <a href="">Log In</a>
+    </div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\KULIAH\KULIAH ODD 2324\WEB PROGRAMMING\LAB\WebProgramming_LAB_Project\resources\views/register.blade.php ENDPATH**/ ?>
